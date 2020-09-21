12cfdaa6e6165ad6475404321c2d6d3843c8b100
sidspears <thesidspears@gmail.com>
2020-09-21 17:18:14 +0300

    Add models and migrations

--- database/migrations/2020_09_21_132644_create_author_book_table.php
+++ database/migrations/2020_09_21_132644_create_author_book_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateAuthorBookTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('author_book_table', function (Blueprint $table) {
+            $table->integer('author_id');
+            $table->integer('book_id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('author_book_table');
     }
 }
